3b455e7a7dd6434d1be0c14651787d2b263717f6
Alison Keppler <aekeppler@gmail.com>
2016-04-06 18:37:13 -0400

    spans

--- js/tap-hover.js
+++ js/tap-hover.js
@@ -10,3 +10,15 @@ $('a.taphover').on("touchstart", function (e) {
         return false; //extra, and to make sure the function has consistent return points
     }
 });
+
+$('span.no-link-hover').on("touchstart", function (e) {
+    "use strict"; //satisfy the code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('portfolio-hover')) {
+        return true;
+    } else {
+        link.addClass("portfolio-hover");
+        $('a.taphover').not(this).removeClass("portfolio-hover");
+        return false; //extra, and to make sure the function has consistent return points
+    }
+});
